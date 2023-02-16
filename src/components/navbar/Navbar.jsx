56b819356dcc248d193f60212ad62fe134b13602
Safi100 <safinafi12@gmail.com>
2023-02-16 12:09:05 +0200

    create open useState

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
+import Hamburger from "../../assets/hamburger-menu.png"
+import Close from "../../assets/icon-close.png"
 const Navbar = () => {
+    const [isOpen, setOpen] = useState(false)
     return (
         <div className='navbar'>
             <div className="wrapper">
                 <h1 className="logo"><a href="/">GPT-3</a></h1>
-                <div className='links_container'>
-                    <div className='close-icon'><span className='close_btn'>x</span></div>
+                <div>
+                    <div className={`hamb_menu ${(isOpen === true) ? 'hide' : '' }`} onClick={()=> setOpen(true)}><span className='open_btn'><img src={Hamburger} alt="Hamburger-menu"/></span></div>
+                    <div className={`close-icon ${(isOpen === false) ? 'hide' : '' }`} onClick={()=> setOpen(false)}><span className='close_btn'><img src={Close} alt="close-icon" /></span></div>
+                </div>
+                <div className={`links_container ${(isOpen === true) ? '' : 'hide' }`}>
                     <ul className='ul_list'>
                         <li><a href="/">Home</a></li>
                         <li><a href="/">What is GPT?</a></li>
@@ -24,4 +30,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default Navbar
