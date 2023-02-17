2266608586521905a153476658d2d4ee4dfc2eee
Safi100 <safinafi12@gmail.com>
2023-02-17 15:28:28 +0200

    rename..

--- src/components/overview/Card.jsx
+++ src/components/overview/Card.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './overview.css'
 const Card = ({heading, body}) => {
     return (
-        <div className='feature'>
+        <div className='card'>
             <div className="line"></div>
-            <h3 className='heading_feature'>{heading}</h3>
-            <p className='body_feature'>{body}</p>
+            <h3 className='heading_card'>{heading}</h3>
+            <p className='body_card'>{body}</p>
         </div>
     )
 }
