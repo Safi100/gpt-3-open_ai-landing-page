8492b5833f76c04cd6a78c92b7b894d27802997f
Safi100 <safinafi12@gmail.com>
2023-02-17 13:26:32 +0200

    create overview copmonent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Companies from './components/companies/Companies'
+import Overview from './components/overview/Overview'
 import './app.css'
 const App = () => {
   return (
     <div className='gradient__bg'>
       <Header />
       <Companies />
+      <div className='wrapper'><Overview /></div> 
     </div>
   )
 }
---
+++ src/components/overview/Overview.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './overview.css'
+import Feature from './feature'
+const Overview = () => {
+    return (
+        <div className='overview'>
+            <div className='row row-1'>
+                <div className='whats'>
+                    <div className="line"></div>
+                    <h3>What is GPT-3</h3>
+                </div>
+                <div className='text_dev'><p className='text text_overview'>We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.</p></div>
+            </div>
+            <div className='row row-2'>
+                <span className='gradient__text heading_overview_text'>The possibilities are beyond your imagination</span>
+                <span className='exp_text'>Explore The Library</span>
+            </div>
+            <div className='row row-3'>
+                <Feature heading={"Chatbots"} body={"We so opinion friends me message as delight. Whole front do of plate heard oh ought. "}/>
+                <Feature heading={"Knowledgebase"} body={"At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"}/>
+                <Feature heading={"Education"} body={"At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"}/>
+            </div>
+        </div>
+    )
+}
+
+export default Overview
